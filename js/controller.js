f74f13d1175b670032997a97efb1dc259ddfbb7e
Benjamin Anderson <Benjamin.k.anderson@gmail.com>
2014-10-30 20:03:48 -0600

    Trying to fix link.

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,7 @@ $(function() {
 
 	// Attack the handler for the pdf copy
 	$('#buy-pdf').on('click', function() {
-		window.location.href = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WZMV5TEV7HXGN' + '&output=embed';
+		window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WZMV5TEV7HXGN');
 	});
 
 	// Attack the handler for the early access copy
